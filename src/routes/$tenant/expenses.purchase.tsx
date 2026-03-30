ccf6ab8b79cb92eaff0cf2c009947a1df26b18a5
agung343 <agungwahydi343@gmail.com>
2026-03-30 17:27:56 +0700

    delete-duplicate-route

--- src/routes/$tenant/expenses.purchase.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/$tenant/expenses/purchase')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/$tenant/expenses/purchase"!</div>
-}
